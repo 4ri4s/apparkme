21d17aeaeddda4101c596cfb745183dc6c905be1
4ri4s <miarias@est.frba.utn.edu.ar>
2020-10-03 19:55:34 -0300

    Agrego estilo inicial para la version web

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -45,6 +45,43 @@ const Login: React.FC = () => {
             <div>
                 <i className="icono"></i>
                 
+                {/* 
+                    LO DE ABAJO ES PARA MODO PC O DE TAMAÑO MAYOR A UN CELULAR
+                */}
+                <div className="dark d-none d-sm-block">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">Apparkme</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        </ul>
+                    </div>
+                    </nav>
+                </div>
+
+                <img className="icon-apparkme-pc" src="http://localhost:8100/assets/icon/logo-apparkame.svg"></img>
+
             </div>
         </IonContent>
     </IonPage>
