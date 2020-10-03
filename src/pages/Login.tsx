7c5824910edc33123939990d058acebbc9228800
4ri4s <miarias@est.frba.utn.edu.ar>
2020-10-03 19:53:49 -0300

    Modifico estilos en version mobile, agrego boton ingresar e imagen de unlam

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,25 +13,38 @@ const Login: React.FC = () => {
   return (
     <IonPage>
         <IonContent fullscreen className="alinear">
-            <div className="dark">
+
+            <div className="dark container d-block d-sm-none" >
                 
+                <br></br>
                 <img src="http://localhost:8100/assets/icon/logo-apparkame.svg"></img>
-                <h5 className="text-context-light" >Bienvenido de vuelta!</h5>
+                <br></br>
+                <br></br>
+                <div className="container">
+                    <h2 className="text-context-light" >Bienvenido de vuelta!</h2>
+                </div>
                 <br></br>
                 <br></br>
                 <form >
-                    <input type="text" placeholder="MAIL O TELEFONO"/>
+                    <input type="text" className="form-control" placeholder="MAIL O TELEFONO"/>
                     <br></br>
-                    <br></br>
-                    <input type="text" placeholder="CONTRASEÑA"/>
+                    <input type="text" className="form-control" placeholder="CONTRASEÑA"/>
                 </form>
-                <Link href="#">
-                    Recuperar contraseña
-                </Link>
-                
+                <a href="#">
+                    OLVIDE MI CONTRASEÑA
+                </a>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <input type="button" className="form-control" value="Ingresar"></input>
+                <br></br>
+                <img className="ico-unlam" src="http://localhost:8100/assets/icon/unlam_logo.png" ></img>
             </div>
+            
             <div>
                 <i className="icono"></i>
+                
             </div>
         </IonContent>
     </IonPage>
