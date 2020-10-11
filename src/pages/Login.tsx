2e59b6e17ee66f84bfd00ff8c88274f6d0f8c4a6
4ri4s <miarias@est.frba.utn.edu.ar>
2020-10-11 15:12:13 -0300

    agrego prueba con hooks conexion con api y un alert como testeo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,34 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { Link, TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
+import axios from 'axios';
 import './Login.css';
 
 const Login: React.FC = () => {
 
-  const { name } = useParams<{ name: string; }>();
+    const [datos, setDatos] = useState({
+        login: '',
+        password: ''
+    })
 
+    const handleInputChange = (e: any) => {
+        setDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const enviarDatos = async (e:any) => {
+        e.preventDefault();
+        const resultado = await axios.post("http://mbar.pythonanywhere.com/login", 
+            datos
+        );
+
+        console.log(resultado);
+        alert(resultado.data.access_token);
+    }
 
   return (
     <IonPage>
@@ -23,20 +43,21 @@ const Login: React.FC = () => {
                         <h2 className="text-context-light" >Bienvenido de vuelta!</h2>
                     </div>
                     <br></br>
-                    <form >
-                        <input type="text" className="form-control" placeholder="MAIL O TELEFONO"/>
+                    <form onSubmit={enviarDatos}>
+                        <input name="login" type="text" onChange={handleInputChange} className="form-control" placeholder="MAIL O TELEFONO"/>
                         <br></br>
-                        <input type="text" className="form-control" placeholder="CONTRASEÑA"/>
-                    </form>
-                    <a href="#">
+                        <input name="password" type="password" onChange={handleInputChange} className="form-control" placeholder="CONTRASEÑA"/>
+                        <a href="#">
                         OLVIDE MI CONTRASEÑA
-                    </a>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <input type="button" className="form-control" value="Ingresar"></input>
-                    <br></br>
+                        </a>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <input type="submit" className="form-control" value="Ingresar"></input>
+                        <br></br>
+                    </form>
+                    
 
                     <footer className="footer">
                         <img className="ico-unlam" src="assets/icon/unlam_logo.png" ></img>
